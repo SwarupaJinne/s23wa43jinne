62063dc2561215c55ac2ca112debce634d971bf5
SwarupaJinne <jinneswarupafall2022@gmail.com>
2023-03-29 18:51:35 -0500

    made changes for bonus

--- routes/computation.js
+++ routes/computation.js
@@ -1,27 +1,18 @@
-const express = require('express');
+var express = require('express');
 var router = express.Router();
-const app = express();
-const port = 3000;
+let x=Math.floor(Math.random());
+let y
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    if(req.query.x!=undefined){
+        x=req.query.x;
 
-app.get('/computation', (req, res) => {
-  // Get the value of x from the query parameter (if available)
-  const x = req.query.x || Math.random();
-
-  // Select a random function from the list
-  const fns = [Math.atan2, Math.atanh, Math.cbrt];
-  const fn = fns[Math.floor(Math.random() * fns.length)];
-
-  // Apply the function to x
-  const y = fn(x);
-
-  // Build the response string
-  const response = `${fn.name} applied to ${x} is ${y}`;
-
-  // Send the response
-  res.send(response);
+    }
+    let r=Math.atan2(x);
+    let t=Math.atanh(x);
+    let n=Math.cbrt(x);
+    
+  res.render('computation', { x:x,atan2:r, atanh:t, cbrt:n});
 });
 
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
 module.exports = router;
