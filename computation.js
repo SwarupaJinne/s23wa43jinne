33077f7c609443d077f7b4aaf7ad08e4efc49fe0
SwarupaJinne <jinneswarupafall2022@gmail.com>
2023-03-29 16:36:34 -0500

    Bonus

---
+++ computation.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/computation', (req, res) => {
+  // Get the value of x from the query parameter (if available)
+  const x = req.query.x || Math.random();
+
+  // Select a random function from the list
+  const fns = [Math.atan2, Math.atanh, Math.cbrt];
+  const fn = fns[Math.floor(Math.random() * fns.length)];
+
+  // Apply the function to x
+  const y = fn(x);
+
+  // Build the response string
+  const response = `${fn.name} applied to ${x} is ${y}`;
+
+  // Send the response
+  res.send(response);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
