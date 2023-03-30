2e813c48f73b0501b61edc544b5039655d901f88
SwarupaJinne <jinneswarupafall2022@gmail.com>
2023-03-29 19:53:12 -0500

    made changes for bonus

--- routes/computation.js
+++ routes/computation.js
@@ -1,19 +1,19 @@
 
 var express = require('express');
 var router = express.Router();
-let value=Math.floor(Math.random());
+let x=Math.floor(Math.random());
 let y
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    if(req.query.value!=undefined){
-        value=req.query.value;
+    if(req.query.x!=undefined){
+        x=req.query.x;
 
     }
-    let p=Math.atan2(value,value);
-    let q=Math.atanh(value);
-    let r=Math.cbrt(value);
+    let p=Math.atan2(x);
+    let q=Math.atanh(x);
+    let r=Math.cbrt(x);
     
-  res.render('computation', { value:value,atan2:p, atanh:q, cbrt:r});
+  res.render('computation', { x:x,atan2:p, atanh:q, cbrt:r});
 });
 
 module.exports = router;
